df90228cc9a67251786b1cb3b962c5553efbdcaf
Gabriel-Nolasco75 <gabriel.govinda.GG@gmail.com>
2024-07-25 17:29:47 -0500

    CRUD familias

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -42,14 +42,29 @@
 
         <div class="mt-14">
 
-            @include('layouts.partials.admin.breadcrumb')
+            <div class="flex justify-between items-center">
+                @include('layouts.partials.admin.breadcrumb')
+
+            @isset($action)
+
+                <div>
+                    {{ $action }}
+                </div>
+
+            @endisset
+                
+            </div>
 
             <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
                 {{ $slot }}
             </div>
         </div>
     </div>
+
+
     @livewireScripts
+
+    @stack('js')
 </body>
 
 </html>
--- resources/views/layouts/partials/admin/sidebar.blade.php
+++ resources/views/layouts/partials/admin/sidebar.blade.php
@@ -4,10 +4,18 @@
             'icon' => 'fa-solid fa-gauge',
             'name' => 'Dashboard',
             'route' => route('admin.dashboard'),
-            'active' => request()->routeIs('admin.dashboard')
-        ]
+            'active' => request()->routeIs('admin.dashboard'),
+        ],
+        [   
+            // Familia de productos
+            'name' => 'Familias',
+            'icon' => 'fa-solid fa-box-open',
+            'route' => route('admin.families.index'),
+            'active' => request()->routeIs('admin.families.*'),
+        ],
     ];
 @endphp
+
 
 <aside id="logo-sidebar"
         class="fixed top-0 left-0 z-40 w-64 h-[100dvh] pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
